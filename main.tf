a95698bc68477305bfb521cfb995b771fc8a116b
makariuz <samuel.eduardo23@gmail.com>
2026-06-04 18:58:16 +0100

    new test after t fmt

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "hcloud_firewall" "waterFirewall" {
       "::/0"
     ]
   }
-    rule {
+  rule {
     direction = "in"
     protocol  = "tcp"
     port      = "80"
@@ -27,7 +27,7 @@ resource "hcloud_firewall" "waterFirewall" {
       "::/0"
     ]
   }
-    rule {
+  rule {
     direction = "in"
     protocol  = "tcp"
     port      = "443"
@@ -39,20 +39,20 @@ resource "hcloud_firewall" "waterFirewall" {
 }
 
 module "hcloud_server" {
-    source = "github.com/Makariuz/up-down//modules/server"
+  source = "github.com/Makariuz/up-down//modules/server"
 
-    hcloud_server = {
-        name = "nextjs-server"
-        image = "ubuntu-22.04"
-        server_type = "cx23"
-        firewall_ids = [hcloud_firewall.waterFirewall.id]
-        owner = "makariuz"
-        user_data = file("scripts/cloud-init.sh")
-    }
+  hcloud_server = {
+    name         = "nextjs-server"
+    image        = "ubuntu-22.04"
+    server_type  = "cx23"
+    firewall_ids = [hcloud_firewall.waterFirewall.id]
+    owner        = "makariuz"
+    user_data    = file("scripts/cloud-init.sh")
+  }
 
-    ssh_key = {
-        name = "nextjs-terraform-key"
-        ssh_key = file("~/.ssh/id_ed25519.pub")
-    }
+  ssh_key = {
+    name    = "nextjs-terraform-key"
+    ssh_key = file("~/.ssh/id_ed25519.pub")
+  }
 
 }
